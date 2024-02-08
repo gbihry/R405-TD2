82350e293a51a2d2a499fa59ee3daf61d2a26526
gbihry <98669194+gbihry@users.noreply.github.com>
2024-02-08 17:11:32 +0100

    etape: 10

--- src/base/main.ts
+++ src/base/main.ts
@@ -1,1 +1,44 @@
 import "@/style/main.css";
+
+
+const myCanvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+const ctx = myCanvas?.getContext("2d");
+const W = window.innerWidth;
+const H = window.innerHeight;
+let x = 0;
+let y = 0;
+
+function initialise(context: CanvasRenderingContext2D) {
+    context.canvas.width = W;
+    context.canvas.height = H;
+}
+
+function afficherRectangle(context: CanvasRenderingContext2D) {
+    context.fillStyle = "red"
+    context.fillRect(50, 50, W / 2, H / 2);
+}
+
+function afficherLigne(context: CanvasRenderingContext2D) {
+
+    context.clearRect(0, 0, W, H);
+
+    x += 2;
+    y += 3;
+
+    context.lineWidth = 15 // changer l'épaisseur du trait
+
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(x + 10, y + 10);
+    context.strokeStyle = "red";
+    context.stroke();
+
+
+    window.requestAnimationFrame(() => afficherLigne(context));
+}
+
+if (ctx) {
+    initialise(ctx)
+    //afficherRectangle(ctx)
+    afficherLigne(ctx)
+}
