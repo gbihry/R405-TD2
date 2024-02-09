473742a3b5df94a07ab8409ccd06d582eb093a2d
gbihry <98669194+gbihry@users.noreply.github.com>
2024-02-09 07:31:07 +0100

    add: particls part 14

--- src/base/main.ts
+++ src/base/main.ts
@@ -1,5 +1,67 @@
 import "@/style/main.css";
 
+const particules = [
+    {
+        x: getRandom(10, 1600),	// Position en X.
+        y: getRandom(10, 800),	// Position en Y.
+        vx: getRandom(10, 800),	// Vitesse sur l’axe X.
+        vy: getRandom(10, 800)	// Vitesse sur l’axe Y.
+    },
+    {
+        x: getRandom(10, 1600),	// Position en X.
+        y: getRandom(10, 800),	// Position en Y.
+        vx: getRandom(10, 800),	// Vitesse sur l’axe X.
+        vy: getRandom(10, 800)	// Vitesse sur l’axe Y.
+    },
+    {
+        x: getRandom(10, 1600),	// Position en X.
+        y: getRandom(10, 800),	// Position en Y.
+        vx: getRandom(10, 800),	// Vitesse sur l’axe X.
+        vy: getRandom(10, 800)	// Vitesse sur l’axe Y.
+    },
+    {
+        x: getRandom(10, 1600),	// Position en X.
+        y: getRandom(10, 800),	// Position en Y.
+        vx: getRandom(10, 800),	// Vitesse sur l’axe X.
+        vy: getRandom(10, 800)	// Vitesse sur l’axe Y.
+    },
+    {
+        x: getRandom(10, 1600),	// Position en X.
+        y: getRandom(10, 800),	// Position en Y.
+        vx: getRandom(10, 800),	// Vitesse sur l’axe X.
+        vy: getRandom(10, 800)	// Vitesse sur l’axe Y.
+    },
+    {
+        x: getRandom(10, 1600),	// Position en X.
+        y: getRandom(10, 800),	// Position en Y.
+        vx: getRandom(10, 800),	// Vitesse sur l’axe X.
+        vy: getRandom(10, 800)	// Vitesse sur l’axe Y.
+    },
+    {
+        x: getRandom(10, 1600),	// Position en X.
+        y: getRandom(10, 800),	// Position en Y.
+        vx: getRandom(10, 800),	// Vitesse sur l’axe X.
+        vy: getRandom(10, 800)	// Vitesse sur l’axe Y.
+    },
+    {
+        x: getRandom(10, 1600),	// Position en X.
+        y: getRandom(10, 800),	// Position en Y.
+        vx: getRandom(10, 800),	// Vitesse sur l’axe X.
+        vy: getRandom(10, 800)	// Vitesse sur l’axe Y.
+    },
+    {
+        x: getRandom(10, 1600),	// Position en X.
+        y: getRandom(10, 800),	// Position en Y.
+        vx: getRandom(10, 800),	// Vitesse sur l’axe X.
+        vy: getRandom(10, 800)	// Vitesse sur l’axe Y.
+    },
+    {
+        x: getRandom(10, 1600),	// Position en X.
+        y: getRandom(10, 800),	// Position en Y.
+        vx: getRandom(10, 800),	// Vitesse sur l’axe X.
+        vy: getRandom(10, 800)	// Vitesse sur l’axe Y.
+    }
+]
 
 const myCanvas = document.getElementById("myCanvas") as HTMLCanvasElement;
 const ctx = myCanvas?.getContext("2d");
@@ -19,21 +81,24 @@ function afficherRectangle(context: CanvasRenderingContext2D) {
 }
 
 function afficherLigne(context: CanvasRenderingContext2D) {
-
     context.clearRect(0, 0, W, H);
 
-    x += 2;
-    y += 3;
+    x += 3;
+    y += 2;
+    particules.forEach(el => {
 
-    context.lineWidth = 15 // changer l'épaisseur du trait
+        context.lineWidth = 15 // changer l'épaisseur du trait
 
-    context.beginPath();
-    context.moveTo(x, y);
-    context.lineTo(x + 10, y + 10);
-    context.strokeStyle = "red";
-    context.stroke();
+        context.beginPath();
+        context.moveTo(el.x + x, el.y + y);
+        context.lineTo(el.x + el.vx, el.y + el.vy);
+        context.strokeStyle = "red";
+        context.stroke();
+
+        console.log(el);
 
 
+    });
     window.requestAnimationFrame(() => afficherLigne(context));
 }
 
@@ -41,4 +106,8 @@ if (ctx) {
     initialise(ctx)
     //afficherRectangle(ctx)
     afficherLigne(ctx)
+}
+
+function getRandom(min: any, max: any) {
+    return Math.random() * (max - min) + min;
 }
